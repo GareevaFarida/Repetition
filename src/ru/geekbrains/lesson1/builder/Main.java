5ebfa0406419296669a7984628ce27fe5003aeaa
GareevaFarida <gareeva.farida@gmail.com>
2020-04-23 14:57:44 +0300

    ПР1

--- src/ru/geekbrains/lesson1/builder/Main.java
+++ src/ru/geekbrains/lesson1/builder/Main.java
@@ -3,12 +3,12 @@ package ru.geekbrains.lesson1.builder;
 public class Main {
     public static void main(String[] args) {
         PersonBuilder builder = new PersonBuilder();
-//        Person person = builder.firstName("Катя")
-//                .lastName("Иванова")
-//                .address("Москва")
-//                .gender("женский")
-//                .age(20).build();
+        Person person = builder.firstName("Катя")
+                .lastName("Иванова")
+                .address("Москва")
+                .gender("женский")
+                .age(20).build();
 
-//        System.out.println(person);
+        System.out.println(person);
     }
 }
